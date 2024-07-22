92e409254708ab01ea1d35547a3e3ac511b97967
Danil <ap.fomin203@gmail.com>
2024-07-22 21:49:23 +0300

    dsdsdsss

--- src/pages/RecipesPage/index.tsx
+++ src/pages/RecipesPage/index.tsx
@@ -1,6 +1,5 @@
 import { RecipesList } from "../../components/Recipeslist";
 import { TitlePage } from "../../components/TitlePage";
-import styles from "./styles.module.scss";
 
 export const RecipesPage = () => {
   return (
