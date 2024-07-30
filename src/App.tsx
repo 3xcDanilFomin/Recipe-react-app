1e65d06bd72210313a76871106442044f49984b0
Danil <ap.fomin203@gmail.com>
2024-07-30 19:28:14 +0300

    update add

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 export const App: React.FC = () => {
+  useEffect(() => {
+    document.body.requestFullscreen();
+  }, []);
   return (
     <div className="container">
       <Outlet />
